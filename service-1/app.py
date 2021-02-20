57357fb6e45d36285b916743f6a7217b55c25c8e
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-20 21:21:53 +0000

    service 1 & 2 working

--- service-1/app.py
+++ service-1/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     # Gets an animal
-    animal = requests.get("http://localhost:5000/animal")
+    animal = requests.get("http://localhost:5001/animal")
     # Gets the noise
-    noise = requests.post("http://localhost:5000/noise", data=animal.text)
+    noise = requests.post("http://localhost:5001/noise", data=animal.text)
 
     return render_template('index.html', animal=animal.text, noise=noise.text)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
